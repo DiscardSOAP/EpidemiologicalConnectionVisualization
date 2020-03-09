67f7e594d8fc355c0d98787dd72fedc4c6ce64f5
xjk <xjk17@mails.tsinghua.edu.cn>
2020-03-09 21:35:00 +0800

    jkxing add session

--- ECVbackend/go.mod
+++ ECVbackend/go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/sessions v0.0.3
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/go-xorm/xorm v0.7.9
--- ECVbackend/handler/helloworld.go
+++ ECVbackend/handler/helloworld.go
@@ -1,6 +1,11 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/gin-contrib/sessions"
+)
 
 func HelloToGuest() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -9,6 +14,12 @@ func HelloToGuest() gin.HandlerFunc {
 }
 func HelloToUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{"msg": "Hello User!"})
+		session := sessions.Default(c)
+		loginuser := session.Get("loginuser")
+		username,ok := loginuser.(string)
+		if !ok {
+			c.JSON(400, gin.H{"msg": "Internal Error"})
+		}
+		c.JSON(200, gin.H{"msg": "Hello user "+username})
 	}
 }
