0abaebb836b0d9eee81ec9bcd971e64c9f890903
fnoi2014xtx <18822300009@163.com>
2020-03-11 11:59:10 +0800

    change config

--- ECVbackend/model/init.go
+++ ECVbackend/model/init.go
@@ -1,31 +1,32 @@
 package model
 
 import (
+	"log"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
-	"log"
 )
 
 var dbEngine *xorm.Engine
 
 func init() {
 	var err error
-	dbEngine, _ = xorm.NewEngine("mysql", "root:123456@/ecv_test?charset=utf8")
+	dbEngine, _ = xorm.NewEngine("mysql", "user:123456@tcp(127.0.0.1:3306)/ecvsql?charset=utf8")
 	//dbEngine.ShowSQL = true
-    dbEngine.ShowSQL(true)
-    dbEngine.SetMaxIdleConns(5)
+	dbEngine.ShowSQL(true)
+	dbEngine.SetMaxIdleConns(5)
 	dbEngine.SetMaxOpenConns(5)
 	err = dbEngine.Ping()
-	if err!=nil{
+	if err != nil {
 		log.Println("ping sql wrong")
-	}else{
+	} else {
 		log.Println("ping sql ok")
 	}
 	dbEngine.CreateTables(&User{})
-	_, err = dbEngine.Insert(&User{Username:"jkxing",Password:"123456"})
-	if err!=nil{
+	_, err = dbEngine.Insert(&User{Username: "jkxing", Password: "123456"})
+	if err != nil {
 		log.Println(err)
-	}else{
+	} else {
 		log.Println("init sql ok")
 	}
 }
